72c7425e85f9a73bf1f21203740171cb1dd22eaf
Ana Ulin <ana@ulin.org>
2019-03-28 17:20:05 -0700

    Try telling selenium-standalone which drivers to install, explicitly.

--- wdio.conf.js
+++ wdio.conf.js
@@ -109,6 +109,14 @@ exports.config = {
     // your test setup with almost no effort. Unlike plugins, they don't add new
     // commands. Instead, they hook themselves up into the test process.
     services: ['selenium-standalone'],
+    // Tell selenium-standalone which drivers to install
+    seleniumInstallArgs: {
+      drivers: {
+        chrome: {
+          arch: process.arch,
+        }
+      }
+    }
     // Framework you want to run your specs with.
     // The following are supported: Mocha, Jasmine, and Cucumber
     // see also: https://webdriver.io/docs/frameworks.html
