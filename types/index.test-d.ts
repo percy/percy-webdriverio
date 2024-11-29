e480cd122f3031f2f7725633794b4df6fbab6048
ninadbstack <60422475+ninadbstack@users.noreply.github.com>
2024-11-29 17:07:12 +0530

    Added support for sync response in typescript (#1240)

--- types/index.test-d.ts
+++ types/index.test-d.ts
@@ -7,10 +7,10 @@ declare const browser: WebdriverIO.Browser;
 expectError(percySnapshot());
 expectError(percySnapshot(browser));
 
-expectType<Promise<void>>(percySnapshot('Snapshot name'));
-expectType<Promise<void>>(percySnapshot(browser, 'Snapshot name'));
-expectType<Promise<void>>(percySnapshot('Snapshot name', { widths: [1000] }));
-expectType<Promise<void>>(percySnapshot(browser, 'Snapshot name', { widths: [1000] }));
+expectType<Promise<void | { [key: string]: any }>>(percySnapshot('Snapshot name'));
+expectType<Promise<void | { [key: string]: any }>>(percySnapshot(browser, 'Snapshot name'));
+expectType<Promise<void | { [key: string]: any }>>(percySnapshot('Snapshot name', { widths: [1000] }));
+expectType<Promise<void | { [key: string]: any }>>(percySnapshot(browser, 'Snapshot name', { widths: [1000] }));
 
 expectError(percySnapshot('Snapshot name', { foo: 'bar' }));
 expectError(percySnapshot(browser, 'Snapshot name', { foo: 'bar' }));
